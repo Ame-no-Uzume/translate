3114c6b16c514a4de9a8b8c82d85b6fb9b9e9e6b
fretfulPig <53642259+fretfulPig@users.noreply.github.com>
2024-04-29 17:52:20 +0700

    Add files via upload

---
+++ precache-manifest.d89475e0e29ddc5e41f822b3718e7484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84174ad349ad238dc7e2108ca10081be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e82f2209808ec1485521",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e40ddf2b9b671016ed71",
+    "url": "/static/js/2.ef63edac.chunk.js"
+  },
+  {
+    "revision": "e82f2209808ec1485521",
+    "url": "/static/js/main.e1c7287a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
